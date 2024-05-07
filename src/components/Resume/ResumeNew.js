91f89e5db64c7da336b079a69204d7fdcf32f873
arbazkhanmadani <arbazkhan03092001@gmail.com>
2024-05-07 15:21:21 +0530

    msg

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Arbaz_Khan_Resume.pdf";
+import pdf from "../../Assets/../Assets/resume.png";
+import pdfd from "../../Assets/../Assets/Arbaz_Khan_resume_updated11-04-24.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -27,25 +28,21 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download My Resume
+            &nbsp;<a href={pdfd} target="_blank" style={{color:"white",textDecoration:"none"}}>Download My Resume</a>
           </Button>
         </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
+         <img type="application/pdf" src={pdf} style={{position:"relative",top:"10px",left:"0px", overflow:"hidden",overflowX:"scroll",width:"80vmin",height:"800px"}}/>
+        
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
             target="_blank"
-            style={{ maxWidth: "250px" }}
+            style={{ maxWidth: "250px",marginTop:"20px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download My Resume
+            &nbsp;<a href={pdfd} target="_blank" style={{color:"white",textDecoration:"none"}}>Download My Resume</a>
           </Button>
         </Row>
       </Container>
